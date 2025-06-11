03f90ce21973a375f01df7d4fd5630e1aa9e709a
EvgeniaK <jeneok0793@gmail.com>
2025-06-12 02:07:14 +0300

    Фикс: правильно обрабатываем webhook

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import logging
 import os
 from aiohttp import web
+
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
+
 from handlers import router
 from config import BOT_TOKEN, WEBHOOK_URL
 
@@ -23,11 +25,17 @@ async def on_shutdown(dispatcher: Dispatcher, bot: Bot):
     await bot.session.close()
     logging.info("🛑 Webhook удалён!")
 
+async def handle_webhook(request):
+    data = await request.json()
+    update = bot.session._client._build_update(data)
+    await dp.feed_update(bot, update)
+    return web.Response()
+
 def main():
     app = web.Application()
     app["bot"] = bot
     app["dp"] = dp
-    app.router.add_post("/webhook", dp.webhook_handler)
+    app.router.add_post("/webhook", handle_webhook)
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
     port = int(os.environ.get("PORT", 8080))
