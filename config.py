3ae1cf9de8db4efb2b532dcf01aeff6400be088b
EvgeniaK <jeneok0793@gmail.com>
2025-06-17 16:38:20 +0300

    🔑 Актуализирован токен после проверки

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "7927473771:AAHjdxRSJ0W0cU0eoGz0E-cb9yZvNZvuHBg"
+BOT_TOKEN = "7927473771:AAGECArPRe9M6C8NaBDNkZ8DhgXkMgrsUDk"
 WEBHOOK_URL = "https://manicure-bot-cloud-764859565904.europe-west3.run.app/webhook"
 SPREADSHEET_ID = "18EeVqLv9BUuwcW-HeMizPMOmoFPrJz7JsVShivC_Pfo"
 GOOGLE_KEY_PATH = "key.json"
