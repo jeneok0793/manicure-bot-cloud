5d0025e56a9542ec9b92513b0562fbc15e278d5f
EvgeniaK <jeneok0793@gmail.com>
2025-06-18 17:41:46 +0300

    🚀 Финальный пуш: конфиг, webhook, PORT, секрет

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = "7927473771:AAGECArPRe9M6C8NaBDNkZ8DhgXkMgrsUDk"
 WEBHOOK_URL = "https://manicure-bot-cloud-764859565904.europe-west3.run.app/webhook"
 SPREADSHEET_ID = "18EeVqLv9BUuwcW-HeMizPMOmoFPrJz7JsVShivC_Pfo"
-GOOGLE_KEY_PATH = "key.json"
+GOOGLE_KEY_PATH = "/secrets/key.json"  # ✅ путь к секрету как в Cloud Run
 ADMIN_CHAT_ID = 7185312775
