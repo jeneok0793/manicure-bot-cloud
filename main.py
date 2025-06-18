bf15a27208bf65c1d5fe0abb638664341d163ac5
EvgeniaK <jeneok0793@gmail.com>
2025-06-18 23:58:51 +0300

    🧪 TEST: простейший aiohttp сервер на 8080

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
-import logging
-import asyncio
-from aiogram import Bot, Dispatcher
-from aiogram.types import Update
 from aiohttp import web
-from config import BOT_TOKEN
-from handlers_base import router  # вместо from handlers import router
 
-
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-dp.include_router(router)
-
-async def handle(request):
-    try:
-        data = await request.json()
-        update = Update.model_validate(data)
-        await dp.feed_update(bot, update)
-        return web.Response()
-    except Exception as e:
-        logging.error(f"❌ Error handling update: {e}")
-        return web.Response(status=500)
+async def hello(request):
+    return web.Response(text="Бот запущен успешно!")
 
 app = web.Application()
-app.router.add_post("/", handle)
+app.router.add_get("/", hello)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     web.run_app(app, port=8080)
