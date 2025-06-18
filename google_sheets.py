fd830a9ad3088f88a7aaa61bb054da5c7e9b1a53
EvgeniaK <jeneok0793@gmail.com>
2025-06-18 15:12:54 +0300

    🐛 FIX: обновлен путь к ключу в google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -2,11 +2,27 @@ import gspread
 from google.oauth2.service_account import Credentials
 from config import SPREADSHEET_ID, GOOGLE_KEY_PATH
 
-def get_service():
-    credentials = Credentials.from_service_account_file(
-        GOOGLE_KEY_PATH,
-        scopes=["https://www.googleapis.com/auth/spreadsheets"]
-    )
-    client = gspread.authorize(credentials)
-    spreadsheet = client.open_by_key(SPREADSHEET_ID)
-    return spreadsheet
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+creds = Credentials.from_service_account_file(GOOGLE_KEY_PATH, scopes=SCOPES)
+client = gspread.authorize(creds)
+sheet = client.open_by_key(SPREADSHEET_ID)
+
+def get_worksheet(name):
+    return sheet.worksheet(name)
+
+def read_cell(sheet_name, cell):
+    return get_worksheet(sheet_name).acell(cell).value
+
+def write_cell(sheet_name, cell, value):
+    get_worksheet(sheet_name).update_acell(cell, value)
+
+def append_row(sheet_name, row):
+    get_worksheet(sheet_name).append_row(row)
+
+def find_row(sheet_name, value):
+    worksheet = get_worksheet(sheet_name)
+    try:
+        cell = worksheet.find(value)
+        return worksheet.row_values(cell.row)
+    except gspread.exceptions.CellNotFound:
+        return None
