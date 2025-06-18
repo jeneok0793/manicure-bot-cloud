7a66efd398882acbb812136ad04c83c18af16e96
EvgeniaK <jeneok0793@gmail.com>
2025-06-19 01:13:33 +0300

    test: aiohttp hello world

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
 FROM python:3.11-slim
-
 WORKDIR /app
-
 COPY . .
-
-RUN pip install --no-cache-dir --upgrade pip && \
-    pip install --no-cache-dir aiohttp
-
+RUN pip install --no-cache-dir -r requirements.txt
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import aiohttp
 from aiohttp import web
 
 async def handle(request):
-    return web.Response(text="I'm alive!")
+    return web.Response(text="Bot is alive!")
 
 app = web.Application()
-app.add_routes([web.get('/', handle)])
+app.add_routes([web.get("/", handle)])
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     web.run_app(app, port=8080)
